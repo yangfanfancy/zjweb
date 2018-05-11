27b00e8878302e76e29e7d244bbae5860f51df6e
yangfan <yangfanfancy@163.com>
2018-05-11 17:32:01 +0800

    提交spring.io生成的springboot文件

---
+++ src/main/java/com/vkeline/zjweb/ZjwebApplication.java
@@ -0,0 +1,12 @@
+package com.vkeline.zjweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZjwebApplication.class, args);
+	}
+}
