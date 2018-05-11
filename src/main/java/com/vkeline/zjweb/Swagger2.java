82abda62d1a3ae5071fe9588fa7cf1d7ddc1bb13
mango <yangfanfancy@163.com>
2018-05-11 22:15:08 +0800

    1.完善swagger2

--- src/main/java/com/vkeline/zjweb/Swagger2.java
+++ src/main/java/com/vkeline/zjweb/Swagger2.java
@@ -10,16 +10,32 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
 public class Swagger2 {
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.didispace.web")).paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                // 指定controller存放的目录路径
+                .apis(RequestHandlerSelectors.basePackage("com.vkeline.zjweb"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("RESTful API文档").description("由Swagger2构建而成").termsOfServiceUrl("").contact("YANG").version("1.0").build();
+        return new ApiInfoBuilder()
+                // 文档标题
+                .title("综检web项目API文档")
+                // 文档描述
+                .description("swagger2")
+                .termsOfServiceUrl("https://github.com/yangfanfancy/zjweb")
+                .version("v1.0")
+                .contact("YANG")
+                .build();
     }
+
 }
 
